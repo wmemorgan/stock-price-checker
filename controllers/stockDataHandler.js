6894dce0c12801b048e1dd2d12bdbd0e1b413dfb
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-09-08 01:07:36 +0800

    Create stock data handler module
    Configure and test API connectivity
    
    Configure and test API data retrieval

---
+++ controllers/stockDataHandler.js
@@ -0,0 +1,20 @@
+const axios = require('axios')
+
+exports.getStockPrice = async (req, res) => {
+  let symbol = req.query.stock.toUpperCase()
+  const stockPriceEndPoint = `https://api.iextrading.com/1.0/stock/${symbol}/price`
+  try {
+    stockPrice = await axios.get(stockPriceEndPoint)
+    let stockData = {
+      stock: symbol,
+      price: stockPrice.data,
+      likes: 0
+    }
+    res.send(stockData)
+  } catch (error) {
+    console.error('Error:', error)
+  }
+}
+
+
+
