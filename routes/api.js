28a4a4aa49d494feb5e8ac28d999f66656fd8e6b
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-09-07 23:57:25 +0800

    Setup mongodb connection

--- routes/api.js
+++ routes/api.js
@@ -13,7 +13,19 @@ var MongoClient = require('mongodb');
 
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
+const stockDataHandler = require('../controllers/stockDataHandler')
+
 module.exports = function (app) {
+  
+  // Connect to database
+  let db
+  MongoClient.connect(CONNECTION_STRING, async (err, conn) => {
+    if (err) throw err
+    else {
+      db = await conn.collection('stockData')
+      console.log(`Successfully connected to: ${CONNECTION_STRING}`)
+    }
+  })
 
   app.route('/api/stock-prices')
     .get(function (req, res){
