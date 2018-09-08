f0f5fd7f5563dd4fcf4c5a028def2767f87a0ffb
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-09-09 01:02:10 +0800

    Remove unnecessary whitespace

--- routes/api.js
+++ routes/api.js
@@ -13,8 +13,6 @@ var expect = require('chai').expect;
 const stockDataHandler = require('../controllers/stockDataHandler')
 
 module.exports = function (app) {
-  
-
 
   app.route('/api/stock-prices')
     .get(stockDataHandler.getStockPrice)
