407069eaf497db162ee75d002a00d7c127012c02
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-09-08 09:50:52 +0800

    Remove Mongodb connectivity from API routes module

--- routes/api.js
+++ routes/api.js
@@ -9,25 +9,14 @@
 'use strict';
 
 var expect = require('chai').expect;
-var MongoClient = require('mongodb');
-
-const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
 const stockDataHandler = require('../controllers/stockDataHandler')
 
 module.exports = function (app) {
   
-  // Connect to database
-  let db
-  MongoClient.connect(CONNECTION_STRING, async (err, conn) => {
-    if (err) throw err
-    else {
-      db = await conn.collection('stockData')
-      console.log(`Successfully connected to: ${CONNECTION_STRING}`)
-    }
-  })
+
 
   app.route('/api/stock-prices')
-    .get(stockDataHandler.getStockPrice);
+    .get(stockDataHandler.getStockPrice)
     
 };
