9bf36c9ae196df4f4b1d531ed80b6330b5816e4b
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-09-08 17:44:43 +0800

    Add retrieval of multiple stock ticker symbols

--- controllers/stockDataHandler.js
+++ controllers/stockDataHandler.js
@@ -22,41 +22,126 @@ mongo.connect(CONNECTION_STRING, async (err, conn) => {
 
 exports.getStockPrice = async (req, res) => {
   console.log(`req.ip is: `, req.ip)
-  let symbol = req.query.stock.toUpperCase()
-  let like = req.query.like ? 1 : 0
-  console.log(`like is : ${like}`)
-  let query = { symbol: symbol }
-  let stockData = []
-  const stockPriceEndPoint = `https://api.iextrading.com/1.0/stock/${symbol}/price`
-  try {
+  let symbol = Array.isArray(req.query.stock) ? req.query.stock : req.query.stock.split(',')
+  let arr = { stockData: [] }
+  for (let i = 0; i < symbol.length ; i++) {
+    let ticker = symbol[i].toUpperCase()
+    console.log(`ticker is: ${ticker}`)
+    let like = symbol[i].like ? 1 : 0
+    console.log(`like is : ${like}`)
+    let query = { symbol: ticker }
+    console.log(`query is: `, query)
+    let stockPriceEndPoint = `https://api.iextrading.com/1.0/stock/${ticker}/price`
+    console.log(`stockPriceEndPoint: ${stockPriceEndPoint}`)
+    try {
     // Retrieve latest stock price
-    const stockPrice = await axios.get(stockPriceEndPoint)
+    let stockPrice = await axios.get(stockPriceEndPoint)
+    console.log(`stockPrice `, stockPrice.data)
     let symbolInfo = await db.findOne(query)
-
+      console.log(`symbolInfo `, symbolInfo.symbol)
     // Upsert stock ticker symbol and like status into database
     if (!symbolInfo || !(symbolInfo.ip.includes(req.ip) && symbolInfo.like > 0)) {
-      console.log('You haven')
       let action = {
         $inc: { like: like },
         $push: { "ip": req.ip }
       }
       db.updateOne(query, action, { upsert: true }, (err, doc) => { if (err) throw err; })
+      console.log(`Add ${symbolInfo} to database`)
       symbolInfo = await db.findOne(query)
+      console.log(`symbolInfo: `, symbolInfo)
     }
-    
-    // Collect and publish stock price and number of likes
-    let stockData = {
-      stockData : {
-        stock: symbolInfo.symbol,
-        price: stockPrice.data,
-        likes: symbolInfo.like
+      let stockData = {
+          stock: symbolInfo.symbol,
+          price: stockPrice.data,
+          likes: symbolInfo.like
       }
-    }
-    console.log(stockData)
-    res.send(stockData)
+      arr.stockData.push(stockData)
+      // console.log(arr)
   } catch (error) {
-    console.error('Error:', error)
+    console.log(error)
+    res.status(500).send(error)
   }
+    // arr.stockData.push(query)
+
+ 
+  }
+
+  console.log(arr)
+  res.send(arr)
+  
+  
+  // res.send(symbol)
+  
+  // let arr = {stockData: []}
+  // for (let i = 0; i < symbol.lenght; i++) {
+  // let ticker = symbol[i].toUpperCase()
+  // console.log(`ticker symbol is: ${symbol}`)
+  // let like = symbol[i].like ? 1 : 0
+  // console.log(`like is : ${like}`)
+  // let query = { symbol: ticker }
+  // console.log(`query is: ${query}`)
+  // let stockPriceEndPoint = `https://api.iextrading.com/1.0/stock/${ticker}/price`
+  // try {
+  //   // Retrieve latest stock price
+  //   const stockPrice = await axios.get(stockPriceEndPoint)
+  //   let symbolInfo = await db.findOne(query)
+
+  //   // Upsert stock ticker symbol and like status into database
+  //   if (!symbolInfo || !(symbolInfo.ip.includes(req.ip) && symbolInfo.like > 0)) {
+  //     let action = {
+  //       $inc: { like: like },
+  //       $push: { "ip": req.ip }
+  //     }
+  //     db.updateOne(query, action, { upsert: true }, (err, doc) => { if (err) throw err; })
+  //     console.log(`Add ${symbolInfo.symbol} to database`)
+  //     symbolInfo = await db.findOne(query)
+  //     let stockData = {
+  //       stockData: {
+  //         stock: symbolInfo.symbol,
+  //         price: stockPrice.data,
+  //         likes: symbolInfo.like
+  //       }
+  //     }
+  //     arr.stockData.push(stockData)
+  //     console.log(arr)
+  //   }
+  // } catch (error) {
+  //   console.log(error)
+  //   res.status(500).send(error)
+  // }
+  // }
+
+  
+  // const stockPriceEndPoint = `https://api.iextrading.com/1.0/stock/${symbol}/price`
+  // try {
+  //   // Retrieve latest stock price
+  //   const stockPrice = await axios.get(stockPriceEndPoint)
+  //   let symbolInfo = await db.findOne(query)
+
+  //   // Upsert stock ticker symbol and like status into database
+  //   if (!symbolInfo || !(symbolInfo.ip.includes(req.ip) && symbolInfo.like > 0)) {
+  //     console.log('You haven')
+  //     let action = {
+  //       $inc: { like: like },
+  //       $push: { "ip": req.ip }
+  //     }
+  //     db.updateOne(query, action, { upsert: true }, (err, doc) => { if (err) throw err; })
+  //     symbolInfo = await db.findOne(query)
+  //   }
+
+    // Collect and publish stock price and number of likes
+    // let stockData = {
+    //   stockData : {
+    //     stock: symbolInfo.symbol,
+    //     price: stockPrice.data,
+    //     likes: symbolInfo.like
+    //   }
+    // }
+  //   console.log(stockData)
+  //   res.send(stockData)
+  // } catch (error) {
+  //   console.error('Error:', error)
+  // }
 }
 
 
